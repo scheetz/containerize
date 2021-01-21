948dfc50790077b0a401fda4ff67735e73f8134f
scheetz <michael_scheetz@yahoo.com>
2021-01-20 18:52:38 -0700

    Fix GH PR version (#4)

--- containerize.sh
+++ containerize.sh
@@ -55,7 +55,7 @@ name_version() {
       version=pr${CIRCLE_PULL_REQUEST##*/}
     fi
     if [ "$GITHUB_EVENT_NAME" = "pull_request" ]; then
-      version=pr${CIRCLE_PULL_REQUEST}
+      version=pr$(echo ${GITHUB_REF} | sed -i -e 's|/merge||g' -e 's|refs/pull/||g')
     fi
     if [ -z $version ] ; then
       version=$(echo $DRONE_TAG | xargs)
